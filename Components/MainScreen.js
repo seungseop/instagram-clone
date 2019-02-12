21b882dec9ebcca72a3870c4c087364b59d26891
Seop <seungseop@users.noreply.github.com>
2019-02-12 18:44:48 +0900

    second commit

--- Components/MainScreen.js
+++ Components/MainScreen.js
@@ -1,6 +1,45 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Icon } from 'native-base'; // 추가된 코드
+import { Text, View, StyleSheet } from 'react-native';
+import { Icon } from 'native-base';
+
+import { createMaterialTopTabNavigator, createAppContainer } from 'react-navigation'; 
+
+// 하단 탭에 들어갈 컴포넌트들
+import HomeTab from './AppTabNavigator/HomeTab'
+import SearchTab from './AppTabNavigator/SearchTab'
+import AddMediaTab from './AppTabNavigator/AddMediaTab'
+import LikesTab from './AppTabNavigator/LikesTab'
+import ProfileTab from './AppTabNavigator/ProfileTab'
+
+// 하단 탭 네비게이터 생성
+const AppTabNavigator = createMaterialTopTabNavigator({
+  HomeTab: { screen: HomeTab },
+  SearchTab: { screen: SearchTab },
+  AddMediaTab: { screen: AddMediaTab },
+  LikesTab: { screen: LikesTab },
+  ProfileTab: { screen: ProfileTab }
+}, {
+  animationEnabled: true,
+  swipeEnabled: true,
+  tabBarPosition: "bottom",
+  tabBarOptions: {
+    style: {
+      ...Platform.select({
+        ios:{
+          backgroundColor:'white',
+        }
+      })
+    },
+    iconStyle: { height: 100 },
+    activeTintColor: '#000',
+    inactiveTintColor: '#d1cece',
+    upperCaseLabel: false,
+    showLabel: false,
+    showIcon: true,
+  }
+});
+
+const AppTabContainer = createAppContainer(AppTabNavigator);
 
 export default class MainScreen extends Component {
   
@@ -12,11 +51,7 @@ export default class MainScreen extends Component {
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>MainScreen</Text>
-      </View>
-    );
+    return <AppTabContainer/>; // AppTabContainer 컴포넌트를 리턴한다.
   }
 }
 
